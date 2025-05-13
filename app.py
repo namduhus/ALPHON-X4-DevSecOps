c572a463724caba22e2d4f5bf646e6da00460815
namduhus <kndh2914@gmail.com>
2025-05-13 14:49:49 +0900

    ngrok연동

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from ALPHON X46667-githubwebook연동테스트 성공"
+    return "Hello from ALPHON X46667-githubwebook연동테스트 성공2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
