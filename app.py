3efb73c3959c3b6d439c79af45e82168f91f743b
namduhus <kndh2914@gmail.com>
2025-05-13 12:19:05 +0900

    [자동화테스트]

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from ALPHON X4"
+    return "Hello from ALPHON X46667"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,14 @@
 docker run -d \
   --name jenkins \
-  -p 8081:8080 -p 50000:50000 \
+  --network host \
+  -u root \
+  -p 8081:8080 \
+  -p 50000:50000 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker \
+  -v /usr/local/bin/kubectl:/usr/local/bin/kubectl \
+  -v /usr/local/bin/k3s:/usr/local/bin/k3s \
+  -v /etc/rancher:/etc/rancher \
+  -v /run/k3s:/run/k3s \
   -v jenkins_home:/var/jenkins_home \
   jenkins/jenkins:lts
